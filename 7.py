280e86ea864f56a42143a21d342ed502aef6eb4b
Artem <Artyom08112006@gmail.com>
2023-03-11 11:24:25 +0300

    added comments for 6th and 7th task

--- 7.py
+++ 7.py
@@ -1,8 +1,15 @@
+# Получаем целое число от пользователя и сохраняем в переменной number
 number = int(input())
+
+# Инициализируем переменную counter значением 0
 counter = 0
 
+# Запускаем цикл от 1 до number + 1
 for i in range(1, number+1):
+    # Проверяем, делится ли number на i без остатка
     if number % i == 0:
+        # Если делится, увеличиваем счетчик counter на 1
         counter += 1
 
+# Выводим значение counter
 print(counter)
